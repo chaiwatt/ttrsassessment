429f50ea922b7e9fafddd5e317e4d85f7976d7d0
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2020-04-26 14:13:07 +0700

    some change

--- config/services.php
+++ config/services.php
@@ -30,8 +30,8 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
     'facebook' => [
-        'client_id' => env('FACEBOOK_CLIENT_ID', '470110740513271'),
-        'client_secret' => env('FACEBOOK_CLIENT_SECRET', '9e155b2c3b58a86ec3153fbd1081022e'),
+        'client_id' => env('FACEBOOK_CLIENT_ID', '167217544577729'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET', '8f94f8e64e75405bb3f3d28752633b7e'),
         'redirect' => env('APP_URL') . 'login/facebook/callback',
     ],
 
