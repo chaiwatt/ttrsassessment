5f8d1fd4d90f9af24018371c9da6f4006cafc1d6
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2020-05-29 17:33:05 +0700

    edit some

--- config/pdf.php
+++ config/pdf.php
@@ -8,7 +8,7 @@ return [
 	'keywords'              => '',
 	'creator'               => 'Laravel Pdf',
 	'display_mode'          => 'fullpage',
-	// 'tempDir'               => base_path('../temp/')
+	'tempDir'               => public_path('temp'),
 	'font_path' => base_path('public/assets/dashboard/fonts/'),
 	'font_data' => [
 		'thsarabunnew' => [
