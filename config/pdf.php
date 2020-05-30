a0be0dc7fafd004675b357fbdf096bf52e7108bd
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2020-05-30 17:46:09 +0700

    work on register

--- config/pdf.php
+++ config/pdf.php
@@ -8,7 +8,7 @@ return [
 	'keywords'              => '',
 	'creator'               => 'Laravel Pdf',
 	'display_mode'          => 'fullpage',
-	'tempDir'               => public_path('temp'),
+	'tempDir'               => base_path('public/temp/'),
 	'font_path' => base_path('public/assets/dashboard/fonts/'),
 	'font_data' => [
 		'thsarabunnew' => [
