263902fefea8489701c21baf9554698e08d66361
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2020-12-14 07:21:11 +0700

    update slide image

--- resources/views/layouts/landing/cookie.blade.php
+++ resources/views/layouts/landing/cookie.blade.php
@@ -13,7 +13,7 @@
       </div>
       <div class="col-sm-2 mt-3">
          {{-- <a href="#" id="btn-cookie" class="  px-2 btn  btn-outline-secondary rounded-pill cookiealert">ตั้งค่าคุกกี้</a> --}}
-         <a href="#" class="px-2 btn  btn-primary rounded-pill btn-cookie acceptcookies">&nbsp; ตกลง &nbsp;</a>
+         <a href="#" class="px-2 btn  btn-primary rounded-pill btn-cookie acceptcookies" style="text-decoration:none" >&nbsp; ตกลง &nbsp;</a>
       </div>
     </div>
   </div>
