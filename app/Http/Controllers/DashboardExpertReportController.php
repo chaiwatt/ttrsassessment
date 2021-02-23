2875cb2d74f46fb3df37eff33107f10e2d0aea4d
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2021-02-23 15:49:14 +0700

    update some

--- app/Http/Controllers/DashboardExpertReportController.php
+++ app/Http/Controllers/DashboardExpertReportController.php
@@ -23,7 +23,7 @@ class DashboardExpertReportController extends Controller
 {
     public function __construct() 
     { 
-        $this->middleware('auth'); 
+        $this->middleware(['auth', 'verified']);
         // $this->middleware('role:3'); 
     }
     public function Index(){
