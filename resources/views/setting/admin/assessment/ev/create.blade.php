367354573f0382bb14a89d30ac6d0fcc0c34b213
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2021-06-04 07:11:46 +0700

    update some

--- resources/views/setting/admin/assessment/ev/create.blade.php
+++ resources/views/setting/admin/assessment/ev/create.blade.php
@@ -56,14 +56,14 @@
                             <fieldset>	
                                 <div class="form-group">
                                     <label>ชื่อ EV</label>
-                                    <input type="text"  name="name" value="{{old('name')}}"  placeholder="ชื่อ EV เช่น ttrs.01" class="form-control form-control-lg">
+                                    <input type="text"  name="name" value="{{old('name')}}"  placeholder="ชื่อ EV" class="form-control form-control-lg">
                                 </div>
                                 <div class="form-group">
                                     <label>เวอร์ชั่น</label>
                                     <input type="text" name="version" value="{{old('version')}}"  placeholder="เวอร์ชั่น" class="form-control form-control-lg">
                                 </div>
                                 <div class="text-right">
-                                    <button type="submit" class="btn bg-teal">บันทึก <i class="icon-paperplane ml-2"></i></button>
+                                    <button type="submit" class="btn bg-teal">บันทึก<i class="icon-paperplane ml-2"></i></button>
                                 </div>
                             </fieldset>
                         </form>
