5f8eef208429dbf014ab7402ba7ebd242b6c7a5c
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2021-09-03 12:05:54 +0700

    update some

--- resources/views/dashboard/admin/realtimereport/project/projectbyisiccode.blade.php
+++ resources/views/dashboard/admin/realtimereport/project/projectbyisiccode.blade.php
@@ -85,7 +85,7 @@
                                                     <td style="text-align: center">{{$fulltbp->minitbp->businessplan->code}}</td>
                                                     <td><a href="{{route('dashboard.admin.report.detail.view',['id' => $fulltbp->minitbp->businessplan->id])}}" class="text-info">{{$fulltbp->minitbp->project}}</a></td>
                                                     <td><a href="{{route('dashboard.admin.search.company.profile',['id' => $fulltbp->minitbp->businessplan->company->id])}}" class="text-info">{{$fulltbp->minitbp->businessplan->company->fullname}}</a> </td>
-                                                    <td style="text-align: center">{{$fulltbp->minitbp->businessplan->company->isic->name}}</td>
+                                                    <td>{{$fulltbp->minitbp->businessplan->company->isic->name}}</td>
                                                 </tr>
                                                 @endif
                                             @endforeach
