a40f43d055caf2ec9b1e06a7c83bc12259e484af
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2021-09-06 11:54:39 +0700

    update some

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -43,19 +43,19 @@
                 <div class="tab-content card-body">
                     <div class="tab-pane fade show active" id="login-tab1">
                         
-                        <div class="text-center mb-3 mb-md-2" style="font-size:40px">
-                            สมัครสมาชิก
+                        <div class="text-center mb-3 mb-md-2" style="font-size:36px">
+                            ผู้ขอรับการประเมิน
                         </div>
         
                     </div>
                     <div class="tab-pane fade" id="login-tab2">
-                        <div class="text-center mb-3 mb-md-2" style="font-size:40px">
-                            สมัครสมาชิก
+                        <div class="text-center mb-3 mb-md-2" style="font-size:36px">
+                            เจ้าหน้าที่ TTRS
                         </div>
                     </div>
                     <div class="tab-pane fade" id="login-tab3">
-                        <div class="text-center mb-3 mb-md-2" style="font-size:40px">
-                            สมัครสมาชิก
+                        <div class="text-center mb-3 mb-md-2" style="font-size:36px">
+                            ผู้เชี่ยวชาญ
                         </div>
                         <div class="form-group mb-3 mb-md-2">
                             <div class="form-check form-check-inline">
