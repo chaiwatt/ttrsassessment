f981ee672a8d0cda464eff15bb1e8d9910f92056
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2021-09-20 08:36:44 +0700

    update some

--- database/seeds/HomepageServiceDefaultsTableSeeder.php
+++ database/seeds/HomepageServiceDefaultsTableSeeder.php
@@ -18,8 +18,8 @@ class HomepageServiceDefaultsTableSeeder extends Seeder
                 'descriptionthai' => 'ผู้ประกอบการยื่นใบสมัครในระบบ TTRS',
                 'descriptioneng' => 'Thai entrepreneurs submit a TTRS application form.',
                 'icon' => '/assets/landing/img/register/1.png',
-                'iconnormal' => '/assets/landing2/images/services/style2/main-img/01.png',
-                'iconhover' => '/assets/landing2/images/services/style2/hover-img/01.png',
+                'iconnormal' => '/assets/landing2/images/services/style2/main-img/0001.png',
+                'iconhover' => '/assets/landing2/images/services/style2/hover-img/0001.png',
                 'cardcolor_id' => 1,
                 'link' => 'servicepage'
             ],
@@ -30,8 +30,8 @@ class HomepageServiceDefaultsTableSeeder extends Seeder
                 'descriptioneng' => 'Complete the form and provide the requested documents.',
                 'icon' => '/assets/landing/img/register/2.png',
                 'link' => 'servicepage',
-                'iconnormal' => '/assets/landing2/images/services/style2/main-img/02.png',
-                'iconhover' => '/assets/landing2/images/services/style2/hover-img/02.png',
+                'iconnormal' => '/assets/landing2/images/services/style2/main-img/0002.png',
+                'iconhover' => '/assets/landing2/images/services/style2/hover-img/0002.png',
                 'cardcolor_id' => 2,
             ],
             [
@@ -41,8 +41,8 @@ class HomepageServiceDefaultsTableSeeder extends Seeder
                 'descriptioneng' => 'The documents will then be verified by specialists.',
                 'icon' => '/assets/landing/img/register/3.png',
                 'link' => 'servicepage',
-                'iconnormal' => '/assets/landing2/images/services/style2/main-img/03.png',
-                'iconhover' => '/assets/landing2/images/services/style2/hover-img/03.png',
+                'iconnormal' => '/assets/landing2/images/services/style2/main-img/0003.png',
+                'iconhover' => '/assets/landing2/images/services/style2/hover-img/0003.png',
                 'cardcolor_id' => 3,
             ],
             [
@@ -52,8 +52,8 @@ class HomepageServiceDefaultsTableSeeder extends Seeder
                 'descriptioneng' => "The entrepreneurs's project is then analyzed and evaluated based on its technology and innovation.",
                 'icon' => '/assets/landing/img/register/4.png',
                 'link' => 'servicepage',
-                'iconnormal' => '/assets/landing2/images/services/style2/main-img/5.png',
-                'iconhover' => '/assets/landing2/images/services/style2/hover-img/5.png',
+                'iconnormal' => '/assets/landing2/images/services/style2/main-img/0004.png',
+                'iconhover' => '/assets/landing2/images/services/style2/hover-img/0004.png',
                 'cardcolor_id' => 4,
             ],
             [
@@ -63,8 +63,8 @@ class HomepageServiceDefaultsTableSeeder extends Seeder
                 'descriptioneng' => "The project is introduced into TTRS assessment system.",
                 'icon' => '/assets/landing/img/register/5.png',
                 'link' => 'servicepage',
-                'iconnormal' => '/assets/landing2/images/services/style2/main-img/05.png',
-                'iconhover' => '/assets/landing2/images/services/style2/hover-img/05.png',
+                'iconnormal' => '/assets/landing2/images/services/style2/main-img/0005.png',
+                'iconhover' => '/assets/landing2/images/services/style2/hover-img/0005.png',
                 'cardcolor_id' => 5,
             ],
             [
@@ -74,8 +74,8 @@ class HomepageServiceDefaultsTableSeeder extends Seeder
                 'descriptioneng' => 'Detemined result and awarded a certificate with a business performance analysis report.',
                 'icon' => '/assets/landing/img/register/6.png',
                 'link' => 'servicepage',
-                'iconnormal' => '/assets/landing2/images/services/style2/main-img/7.png',
-                'iconhover' => '/assets/landing2/images/services/style2/hover-img/7.png',
+                'iconnormal' => '/assets/landing2/images/services/style2/main-img/0007.png',
+                'iconhover' => '/assets/landing2/images/services/style2/hover-img/0007.png',
                 'cardcolor_id' => 6,
             ] 
         ]);
