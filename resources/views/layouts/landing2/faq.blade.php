0664f6e062813f484dad7613f580a16740898a83
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2021-10-04 15:01:03 +0700

    update some

--- resources/views/layouts/landing2/faq.blade.php
+++ resources/views/layouts/landing2/faq.blade.php
@@ -15,7 +15,7 @@
                <div class="faq-content">
                    <div id="accordion" class="accordion">
                        @foreach ($sharefaqs as $key => $faq)
-                       <div class="card" data-aos="fade-right" data-aos-delay="{{($key + 1)*100}}">
+                       <div class="card" data-aos="fade-right" data-aos-delay="{{($key + 1)*100}}" style="margin-top: 15px">
                             <div class="card-header">
                                 <a class="card-link collapsed" data-toggle="collapse" href="#collapse{{$key}}"  aria-expanded="false" >
                                     @if (Config::get('app.locale') == 'th')
