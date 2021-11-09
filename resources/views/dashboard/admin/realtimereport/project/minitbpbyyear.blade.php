ff4997617e3803cb3ae0cdea0e3a2f4690be5e0b
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2021-11-09 08:13:04 +0700

    update some

--- resources/views/dashboard/admin/realtimereport/project/minitbpbyyear.blade.php
+++ resources/views/dashboard/admin/realtimereport/project/minitbpbyyear.blade.php
@@ -38,7 +38,7 @@
                 <div class="card">
                     <input id="attendeventid" type="text" hidden>
                     <div class="card-header header-elements-sm-inline">
-                        <h6 class="card-title" style="font-size:16px;font-weight: bold">โครงการที่ยื่น Mini TBP รายปี</h6>
+                        <h6 class="card-title" style="font-size:16px;font-weight: bold">โครงการยื่น Mini TBP รายปี</h6>
                         <div class="header-elements">
                             {{-- <a class="text-default daterange font-weight-semibold cursor-pointer dropdown-toggle">
                             </a> --}}
